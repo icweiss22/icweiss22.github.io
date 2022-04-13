7ad0f8da86f531b3e904b22c6bc24d5cf34e4e0b
Isaac Weiss <icw0001@auburn.edu>
2022-04-13 16:22:14 -0500

    release 1

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
     AOS.init();
+
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#sideNav'
+    })
+
+
 });
+
+window.addEventListener('scroll', function () {
+    if (this.scrollY === 0) {
+        $('#about-nav-link').addClass('active');
+    }
+
+})
 
